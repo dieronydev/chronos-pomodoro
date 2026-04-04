6b87a1615a336496e262acd1ba98e2eb3d78233d
dieronydev <dieronydev@gmail.com>
2026-04-04 20:33:47 -0300

    feat: implementa layout do input de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 
 export function App() {
   return (
@@ -19,6 +20,32 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <DefaultInput
+              id="input"
+              type="text"
+              labelText="task"
+              placeholder="Digite a tarefa aqui"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
